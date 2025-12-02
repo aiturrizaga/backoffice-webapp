fec1202a055ba3715d66d7105433b1444436c4a5
Alvaro Iturrizaga Vargas <aiv@wowperu.pe>
2025-12-01 20:08:21 -0500

    Update settings page

--- src/app/feature/setting/pages/setting-home/setting-home.ts
+++ src/app/feature/setting/pages/setting-home/setting-home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Page } from '@ui/components/page';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-setting-home',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatTabsModule,
+    Page
+  ],
   templateUrl: './setting-home.html',
   styles: ``,
 })
 export class SettingHome {
-
+  links = ['Empresa', 'Sucursales', 'Terminales', 'Seriales', 'Empleados', 'Roles', 'Cobros', 'Tipo de cambio'];
+  activeLink = this.links[0];
 }
